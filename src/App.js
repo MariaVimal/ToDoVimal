b8349b6ca113ed481f685b350b8dc385a06212d9
VimalVjy <mvimal710@gmail.com>
2019-08-14 09:45:50 +0530

    Header & Footer Finished

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+import ReactDoOM from 'react-dom'
+
+function App(){
+    return <h1>Good Morning Team</h1>
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDoOM from 'react-dom'
+import App from './App'
+
+// function App(){
+//     return <h1>Good Morning Team</h1>
+// }
+
+ReactDoOM.render(<App/>,document.getElementById('root'))
